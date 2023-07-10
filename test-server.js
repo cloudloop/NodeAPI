891bae6b0cfa75c57ca1c84bac79aeac2701f5b9
Axel Ingo <axel.k.ingo@gmail.com>
2023-07-10 11:25:28 +0200

    experiment with https

--- test-server.js
+++ test-server.js
@@ -1,23 +1,30 @@
+const https = require('https');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 5000;
 
 // Set EJS as the template engine
-app.set('view engine', 'ejs');
+//app.set('view engine', 'ejs');
 
 // Define a route that renders the HTML
 app.get('/', async (req, res) => {
-    process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
     // Fetch data from an API (replace this URL with the actual API endpoint)
-    const apiUrl = 'https://dummyjson.com/products';
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-    console.log(data.products)
+    // const apiUrl = 'https://dummyjson.com/products';
+    // const response = await fetch(apiUrl);
+    // const data = await response.json();
+    // console.log(data.products)
 
     // Render the EJS template with the data
-    res.render('index', { products: data.products });
+    //res.render('index', { products: data.products });
+    res.json({ hello: "World" });
 });
 
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
+const options = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem')
+};
+
+https.createServer(options, app).listen(port, () => {
+    console.log(`Server listening at https://localhost:${port}`);
 });
