6e43c00e686f49ee5c816e4ec36a014034dd8998
Axel Ingo <axel.k.ingo@gmail.com>
2023-07-13 15:24:38 +0200

    server2 and weather.ejs updated to reflect env

--- S2/server2.js
+++ S2/server2.js
@@ -14,22 +14,22 @@ app.set('view engine', 'ejs');
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, '../public')));
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '1';
 
 
 app.get('/api/call-server1', async (req, res) => {
     try {
-        const response = await axios.get('http://localhost:3000/api/weather/place/Stockholm');
-        // console.log(response.data.weatherData.properties.timeseries);
-        //res.json({ message: 'Received from Server 1:', data: response.data });
-
-        console.log(response)
-
-        // Render the EJS template with the data
-        //res.render('weather', { times: response.data.weatherData.properties.timeseries  });
-
+        const response = await axios.get('http://localhost:3000/api/weatherdata/place/Stockholm');
+        console.log(response);
+        res.render('weather', { times: response.data.weatherData.properties.timeseries, response: response.data.weatherData });
     } catch (error) {
-        res.status(500).json({ message: 'Error calling Server 1', });//error: error.response.data });
+        console.error(error);
+        if (error.response) {
+            res.status(500).json({ message: 'Error calling Server 1', error: error.response.data });
+        } else {
+            // The error is not due to an HTTP response, so just return the entire error object
+            res.status(500).json({ message: 'Error calling Server 1', error: error.message });
+        }
     }
 });
 
