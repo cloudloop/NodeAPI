abd175dfcf02871535e2de200160106729ee06cb
Axel Ingo <axel.k.ingo@gmail.com>
2023-07-17 23:06:34 +0200

    firebase activated. Firestore test working!

--- S1/server1.js
+++ S1/server1.js
@@ -1,20 +1,77 @@
-const express = require('express');
-const axios = require('axios');
-const path = require('path');
+import express from 'express';
+import axios from 'axios';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
+// Configure server and its settings:
 const app = express();
 const port = 3000;
-
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, '../public')));
-
 // Set EJS as the template engine
 app.set('view engine', 'ejs');
-
 // Set Express to pretty-print JSON
 app.set('json spaces', 2);
-
+//Supress the https warning
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+
+// Firebase initialization
+// Firebase compat packages are API compatible with namespaced code
+import {initializeApp} from 'firebase/app';
+import {getAuth} from 'firebase/auth';
+import {getFirestore, collection, addDoc, setDoc} from 'firebase/firestore';
+import { getAnalytics } from "firebase/analytics";
+import { write } from 'fs';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCZWPVc1B4TiZ22fpvpZuCQS00lXfUYtoU",
+  authDomain: "kiteweather-d40b6.firebaseapp.com",
+  databaseURL: "https://kiteweather-d40b6-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "kiteweather-d40b6",
+  storageBucket: "kiteweather-d40b6.appspot.com",
+  messagingSenderId: "320809676319",
+  appId: "1:320809676319:web:2926b3d4adba188c876f2a",
+  measurementId: "G-CGBLL2J1LF",
+  databaseURL: "https://kiteweather-d40b6-default-rtdb.europe-west1.firebasedatabase.app/"
+};
+
+// Initialize Firebase
+const FBapp = initializeApp(firebaseConfig);
+//const analytics.isSupported() = getAnalytics(FBapp);
+const db = getFirestore(FBapp);
+async function writeData(PlaceName, data) {
+    console.log('writeData function called');
+
+    const postData = {
+        place: PlaceName,
+        latestUpdate: data.properties.meta.updated_at,
+        Weatherdata : data
+    };
+    // try {
+    //   console.log(`Trying to overwrite existing document...`);
+    //   await setDoc(doc(db, 'weather', 'someId'), postData);
+    //   console.log("Overwrote existing document in the database ...");
+    //   console.log(postData);
+    // } catch (error) {
+    //   console.log(`Overwriting failed. Creating new document...`);
+    try {
+    await addDoc(collection(db, 'w2'), postData);
+    console.log("Created new document in the database ...");
+    console.log(postData);
+    } catch (error) {
+    console.log(`Error writing to database: `, error);
+    }
+}
+
+
+
 
 app.get('/api/greeting', (req, res) => {
     res.json({ message: 'Hello from Server 1!' });
@@ -85,6 +142,7 @@ app.get('/api/weatherdata/place/:place', async (req, res) => {
     if (coordinates) {
         const weatherData = await fetchWeatherData(coordinates.lat, coordinates.lon);
         if (weatherData) {
+            writeData(place, weatherData);
             res.json({ placeName: place, latitude: coordinates.lat, longitude: coordinates.lon, weatherData });
         } else {
             res.status(500).json({ message: 'Error fetching weather data' });
